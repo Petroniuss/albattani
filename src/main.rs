60f672e6ec5ee0d0d2bd468f2e4933d73ef2e4c4
Patryk Wojtyczek <pwojtyczek@qualtrics.com>
2022-11-06 21:38:35 +0100

    switch to kiss3d

--- src/main.rs
+++ src/main.rs
@@ -1,287 +1,53 @@
-use std::sync::Arc;
+extern crate kiss3d;
 
-struct EguiExampleData {
-    _object_handle: rend3::types::ObjectHandle,
-    material_handle: rend3::types::MaterialHandle,
-    _directional_handle: rend3::types::DirectionalLightHandle,
+use kiss3d::light::Light;
+use kiss3d::scene::SceneNode;
+use kiss3d::window::{State, Window};
+use kiss3d::nalgebra::{UnitQuaternion, Vector3};
+use nalgebra::Translation3;
 
-    egui_routine: rend3_egui::EguiRenderRoutine,
-    platform: egui_winit_platform::Platform,
-    start_time: instant::Instant,
-    color: [f32; 4],
+
+// all right, this looks like it's exactly what I want :)
+
+/*
+
+API:
+- load a bunch of nodes and edges
+- somehow highlight the node/edge?
+
+*/
+
+struct AppState {
+    c: Vec<SceneNode>,
+    rot: UnitQuaternion<f32>,
 }
 
-const SAMPLE_COUNT: rend3::types::SampleCount = rend3::types::SampleCount::One;
-
-#[derive(Default)]
-struct EguiExample {
-    data: Option<EguiExampleData>,
-}
-impl rend3_framework::App for EguiExample {
-    const HANDEDNESS: rend3::types::Handedness = rend3::types::Handedness::Left;
-
-    fn sample_count(&self) -> rend3::types::SampleCount {
-        SAMPLE_COUNT
-    }
-
-    fn setup(
-        &mut self,
-        window: &winit::window::Window,
-        renderer: &Arc<rend3::Renderer>,
-        _routines: &Arc<rend3_framework::DefaultRoutines>,
-        surface_format: rend3::types::TextureFormat,
-    ) {
-        let window_size = window.inner_size();
-
-        // Create the egui render routine
-        let egui_routine = rend3_egui::EguiRenderRoutine::new(
-            renderer,
-            surface_format,
-            rend3::types::SampleCount::One,
-            window_size.width,
-            window_size.height,
-            window.scale_factor() as f32,
-        );
-
-        // Create mesh and calculate smooth normals based on vertices
-        let mesh = create_mesh();
-
-        // Add mesh to renderer's world.
-        //
-        // All handles are refcounted, so we only need to hang onto the handle until we
-        // make an object.
-        let mesh_handle = renderer.add_mesh(mesh);
-
-        // Add PBR material with all defaults except a single color.
-        let material = rend3_routine::pbr::PbrMaterial {
-            albedo: rend3_routine::pbr::AlbedoComponent::Value(glam::Vec4::new(0.0, 0.5, 0.5, 1.0)),
-            transparency: rend3_routine::pbr::Transparency::Blend,
-            ..rend3_routine::pbr::PbrMaterial::default()
-        };
-        let material_handle = renderer.add_material(material);
-
-        // Combine the mesh and the material with a location to give an object.
-        let object = rend3::types::Object {
-            mesh_kind: rend3::types::ObjectMeshKind::Static(mesh_handle),
-            material: material_handle.clone(),
-            transform: glam::Mat4::IDENTITY,
-        };
-
-        // Creating an object will hold onto both the mesh and the material
-        // even if they are deleted.
-        //
-        // We need to keep the object handle alive.
-        let _object_handle = renderer.add_object(object);
-
-        let camera_pitch = std::f32::consts::FRAC_PI_4;
-        let camera_yaw = -std::f32::consts::FRAC_PI_4;
-        // These values may seem arbitrary, but they center the camera on the cube in
-        // the scene
-        let camera_location = glam::Vec3A::new(5.0, 7.5, -5.0);
-        let view = glam::Mat4::from_euler(glam::EulerRot::XYZ, -camera_pitch, -camera_yaw, 0.0);
-        let view = view * glam::Mat4::from_translation((-camera_location).into());
-
-        // Set camera location data
-        renderer.set_camera_data(rend3::types::Camera {
-            projection: rend3::types::CameraProjection::Perspective { vfov: 60.0, near: 0.1 },
-            view,
-        });
-
-        // Create a single directional light
-        //
-        // We need to keep the directional light handle alive.
-        let _directional_handle = renderer.add_directional_light(rend3::types::DirectionalLight {
-            color: glam::Vec3::ONE,
-            intensity: 10.0,
-            // Direction will be normalized
-            direction: glam::Vec3::new(-1.0, -4.0, 2.0),
-            distance: 400.0,
-        });
-
-        // Create the winit/egui integration, which manages our egui context for us.
-        let platform = egui_winit_platform::Platform::new(egui_winit_platform::PlatformDescriptor {
-            physical_width: window_size.width as u32,
-            physical_height: window_size.height as u32,
-            scale_factor: window.scale_factor(),
-            font_definitions: egui::FontDefinitions::default(),
-            style: Default::default(),
-        });
-
-        let start_time = instant::Instant::now();
-        let color: [f32; 4] = [0.0, 0.5, 0.5, 1.0];
-
-        self.data = Some(EguiExampleData {
-            _object_handle,
-            material_handle,
-            _directional_handle,
-
-            egui_routine,
-            platform,
-            start_time,
-            color,
-        })
-    }
-
-    fn handle_event(
-        &mut self,
-        window: &winit::window::Window,
-        renderer: &Arc<rend3::Renderer>,
-        routines: &Arc<rend3_framework::DefaultRoutines>,
-        base_rendergraph: &rend3_routine::base::BaseRenderGraph,
-        surface: Option<&Arc<rend3::types::Surface>>,
-        resolution: glam::UVec2,
-        event: rend3_framework::Event<'_, ()>,
-        control_flow: impl FnOnce(winit::event_loop::ControlFlow),
-    ) {
-        let data = self.data.as_mut().unwrap();
-
-        // Pass the winit events to the platform integration.
-        data.platform.handle_event(&event);
-
-        match event {
-            rend3_framework::Event::RedrawRequested(..) => {
-                data.platform.update_time(data.start_time.elapsed().as_secs_f64());
-                data.platform.begin_frame();
-
-                // Insert egui commands here
-                let ctx = data.platform.context();
-                egui::Window::new("Change color").resizable(true).show(&ctx, |ui| {
-                    ui.label("Change the color of the cube");
-                    if ui.color_edit_button_rgba_unmultiplied(&mut data.color).changed() {
-                        renderer.update_material(
-                            &data.material_handle.clone(),
-                            rend3_routine::pbr::PbrMaterial {
-                                albedo: rend3_routine::pbr::AlbedoComponent::Value(glam::Vec4::from(data.color)),
-                                transparency: rend3_routine::pbr::Transparency::Blend,
-                                ..rend3_routine::pbr::PbrMaterial::default()
-                            },
-                        );
-                    }
-                });
-
-                // End the UI frame. Now let's draw the UI with our Backend, we could also
-                // handle the output here
-                let (_output, paint_commands) = data.platform.end_frame(Some(window));
-                let paint_jobs = data.platform.context().tessellate(paint_commands);
-
-                let input = rend3_egui::Input {
-                    clipped_meshes: &paint_jobs,
-                    context: data.platform.context(),
-                };
-
-                // Get a frame
-                let frame = rend3::util::output::OutputFrame::Surface {
-                    surface: Arc::clone(surface.unwrap()),
-                };
-
-                // Ready up the renderer
-                let (cmd_bufs, ready) = renderer.ready();
-
-                // Lock the routines
-                let pbr_routine = rend3_framework::lock(&routines.pbr);
-                let tonemapping_routine = rend3_framework::lock(&routines.tonemapping);
-
-                // Build a rendergraph
-                let mut graph = rend3::graph::RenderGraph::new();
-
-                // Add the default rendergraph without a skybox
-                base_rendergraph.add_to_graph(
-                    &mut graph,
-                    &ready,
-                    &pbr_routine,
-                    None,
-                    &tonemapping_routine,
-                    resolution,
-                    SAMPLE_COUNT,
-                    glam::Vec4::ZERO,
-                );
-
-                // Add egui on top of all the other passes
-                let surface = graph.add_surface_texture();
-                data.egui_routine.add_to_graph(&mut graph, input, surface);
-
-                // Dispatch a render using the built up rendergraph!
-                graph.execute(renderer, frame, cmd_bufs, &ready);
-
-                control_flow(winit::event_loop::ControlFlow::Poll);
-            }
-            rend3_framework::Event::MainEventsCleared => {
-                window.request_redraw();
-            }
-            rend3_framework::Event::WindowEvent { event, .. } => match event {
-                winit::event::WindowEvent::Resized(size) => {
-                    data.egui_routine
-                        .resize(size.width, size.height, window.scale_factor() as f32);
-                }
-                winit::event::WindowEvent::CloseRequested => {
-                    control_flow(winit::event_loop::ControlFlow::Exit);
-                }
-                _ => {}
-            },
-            _ => {}
+impl State for AppState {
+    fn step(&mut self, _: &mut Window) {
+        for e in self.c.iter_mut() {
+            e.prepend_to_local_rotation(&self.rot)
         }
     }
 }
 
 fn main() {
-    let app = EguiExample::default();
-    rend3_framework::start(
-        app,
-        winit::window::WindowBuilder::new()
-            .with_title("egui")
-            .with_maximized(true),
-    )
-}
+    let mut window = Window::new("Kiss3d: wasm example");
 
-fn vertex(pos: [f32; 3]) -> glam::Vec3 {
-    glam::Vec3::from(pos)
-}
+    let mut nodes = vec![];
+    for x in 0..10 {
+        let xx = (x as f32) * 2.0;
+        let mut c = window.add_cube(1.0, 1.0, 1.0);
+        c.append_translation(&Translation3::new(xx, xx, xx));
+        c.set_color(1.0, 0.0, 0.0);
 
-fn create_mesh() -> rend3::types::Mesh {
-    let vertex_positions = [
-        // far side (0.0, 0.0, 1.0)
-        vertex([-1.0, -1.0, 1.0]),
-        vertex([1.0, -1.0, 1.0]),
-        vertex([1.0, 1.0, 1.0]),
-        vertex([-1.0, 1.0, 1.0]),
-        // near side (0.0, 0.0, -1.0)
-        vertex([-1.0, 1.0, -1.0]),
-        vertex([1.0, 1.0, -1.0]),
-        vertex([1.0, -1.0, -1.0]),
-        vertex([-1.0, -1.0, -1.0]),
-        // right side (1.0, 0.0, 0.0)
-        vertex([1.0, -1.0, -1.0]),
-        vertex([1.0, 1.0, -1.0]),
-        vertex([1.0, 1.0, 1.0]),
-        vertex([1.0, -1.0, 1.0]),
-        // left side (-1.0, 0.0, 0.0)
-        vertex([-1.0, -1.0, 1.0]),
-        vertex([-1.0, 1.0, 1.0]),
-        vertex([-1.0, 1.0, -1.0]),
-        vertex([-1.0, -1.0, -1.0]),
-        // top (0.0, 1.0, 0.0)
-        vertex([1.0, 1.0, -1.0]),
-        vertex([-1.0, 1.0, -1.0]),
-        vertex([-1.0, 1.0, 1.0]),
-        vertex([1.0, 1.0, 1.0]),
-        // bottom (0.0, -1.0, 0.0)
-        vertex([1.0, -1.0, 1.0]),
-        vertex([-1.0, -1.0, 1.0]),
-        vertex([-1.0, -1.0, -1.0]),
-        vertex([1.0, -1.0, -1.0]),
-    ];
+        nodes.push(c);
+    }
 
-    let index_data: &[u32] = &[
-        0, 1, 2, 2, 3, 0, // far
-        4, 5, 6, 6, 7, 4, // near
-        8, 9, 10, 10, 11, 8, // right
-        12, 13, 14, 14, 15, 12, // left
-        16, 17, 18, 18, 19, 16, // top
-        20, 21, 22, 22, 23, 20, // bottom
-    ];
 
-    rend3::types::MeshBuilder::new(vertex_positions.to_vec(), rend3::types::Handedness::Left)
-        .with_indices(index_data.to_vec())
-        .build()
-        .unwrap()
+    window.set_light(Light::StickToCamera);
+
+    let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
+    let state = AppState { c: nodes, rot };
+
+    window.render_loop(state)
 }
