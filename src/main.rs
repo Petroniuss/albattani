15797903d4a28a0c47dfb0870189a5b168b4a11f
Patryk Wojtyczek <pwojtyczek@qualtrics.com>
2022-11-10 15:30:53 +0100

    split into modules

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,17 @@
-extern crate kiss3d;
+mod simulation;
+mod simulation_renderer;
 
-use std::cell::Ref;
-use std::cell::RefCell;
-use std::rc::Rc;
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
-use std::thread::sleep;
-use std::time::Duration;
 
-use kiss3d::nalgebra::{UnitQuaternion, Vector3};
-use kiss3d::scene::SceneNode;
-use kiss3d::window::{State, Window};
-
-use nalgebra::Point3;
-use nalgebra::Rotation3;
-use nalgebra::Translation3;
+use kiss3d::window::Window;
 
 use color_eyre::eyre::eyre;
 use color_eyre::Result;
-use log::{info, LevelFilter};
+use log::LevelFilter;
 
-use rand::{thread_rng, Rng};
-use random_color::{Color, Luminosity, RandomColor};
-
-use tap::Tap;
+use crate::simulation::{Graph, SimpleSimulation, Simulation, SimulationParameters, SimulationUpdate};
+use crate::simulation_renderer::SimulationRenderer;
 
 // ----------------------------------------------------------
 //                    Open Questions
@@ -53,83 +41,6 @@ Rendering API:
     This has to be indicated by simulation I guess.
 
 */
-// ----------------------------------------------------------
-//                    Simulation
-// ----------------------------------------------------------
-
-type VertexId = usize;
-
-// might carry some additional data.
-#[derive(Clone, Copy, Debug)]
-struct Edge {
-    from: usize,
-    to: usize,
-}
-
-// might carry some additional data.
-#[derive(Clone, Copy, Debug)]
-#[allow(dead_code)]
-struct Vertex {
-    id: VertexId,
-}
-
-#[allow(dead_code)]
-#[derive(Clone, Debug)]
-struct Graph {
-    vertices: Vec<Vertex>,
-    edges: Vec<Vec<Edge>>,
-}
-
-impl Graph {
-    fn construct_graph(n: usize, edges_iter: impl IntoIterator<Item = (usize, usize)>) -> Graph {
-        let vertices = (0..n).into_iter().map(|id| Vertex { id }).collect();
-
-        let mut edges = Vec::with_capacity(n).tap_mut(|edges| {
-            for _ in 0..n {
-                edges.push(Vec::new());
-            }
-        });
-
-        for (from, to) in edges_iter.into_iter() {
-            edges[from].push(Edge { from, to });
-        }
-
-        Graph { vertices, edges }
-    }
-}
-
-enum SimulationUpdate {
-    EdgeSelected(Edge),
-}
-
-struct Simulation {
-    graph: Graph,
-    simulation_update_sender: Sender<SimulationUpdate>,
-}
-
-impl Simulation {
-    fn start_simulation(&mut self) -> Result<()> {
-        let mut selected_edge = self.graph.edges[0][0];
-
-        loop {
-            if let Err(_err) = self
-                .simulation_update_sender
-                .send(SimulationUpdate::EdgeSelected(selected_edge))
-            {
-                return Ok(());
-            }
-
-            info!("Selected: {:?}", selected_edge);
-            sleep(Duration::from_secs(2));
-
-            let edges_num = self.graph.edges[selected_edge.to].len();
-            let next_edge_idx = thread_rng().gen_range(0..edges_num);
-            let next_edge = self.graph.edges[selected_edge.to][next_edge_idx];
-
-            selected_edge = next_edge;
-        }
-    }
-}
 
 fn test_graph() -> Graph {
     Graph::construct_graph(5, vec![(0, 1), (1, 2), (2, 3), (3, 0), (1, 4), (4, 0)])
@@ -154,150 +65,6 @@ fn test_graph() -> Graph {
 // while the renderer might have to poll the queue or something.
 // sounds cool!
 
-#[allow(dead_code)]
-struct SimulationRenderer {
-    // communication with simulation.
-    simulation_update_rx: Receiver<SimulationUpdate>,
-
-    // steady state (it rarely changes)
-    vertex_nodes: Vec<Rc<RefCell<SceneNode>>>,
-    edge_nodes: Vec<Vec<(VertexId, Rc<RefCell<SceneNode>>)>>,
-
-    // temporary state
-    highlighted_edge: Option<Rc<RefCell<SceneNode>>>,
-}
-
-static GRAPH_NODE_RADIUS: f32 = 0.45;
-static DEFAULT_EDGE_COLOR: [f32; 3] = [0.8, 0.1, 0.2];
-static HIGHLIGHT_EDGE_COLOR: [f32; 3] = [0.2, 0.8, 0.2];
-static EDGE_WIDTH: f32 = 0.01;
-
-fn gen_random_color(_node_id: usize) -> [f32; 3] {
-    let [r, g, b] = RandomColor::new()
-        .hue(Color::Purple) // Optional
-        .luminosity(Luminosity::Light)
-        .alpha(1.0) // Optional
-        .to_rgb_array();
-
-    let norm = |v: u8| v as f32 / 255.0;
-    [norm(r), norm(g), norm(b)]
-}
-
-impl SimulationRenderer {
-    fn from_graph(
-        graph: &Graph,
-        rx: Receiver<SimulationUpdate>,
-        window: &mut Window,
-    ) -> SimulationRenderer {
-        // TODO: we need to find positions for every node. somehow?
-        // something sophisticated - force_graph https://docs.rs/force_graph/latest/force_graph/?
-        // and something less sophisticated :)
-        let mut vertex_nodes = vec![];
-        for id in 0..graph.vertices.len() {
-            let mut vertex_node = window.add_sphere(GRAPH_NODE_RADIUS);
-            let x = id as f32;
-            let y = (id % 2) as f32;
-            let z = (id % 3) as f32 * 2.0;
-            vertex_node.append_translation(&Translation3::new(x, y, z));
-
-            let [r, g, b] = gen_random_color(id);
-            vertex_node.set_color(r, g, b);
-
-            vertex_nodes.push(Rc::new(RefCell::new(vertex_node)));
-        }
-
-        let mut edge_nodes = vec![];
-        for _id in 0..graph.vertices.len() {
-            edge_nodes.push(vec![]);
-        }
-
-        let compute_position = |node_ref: Ref<SceneNode>| {
-            let position = node_ref
-                .data()
-                .local_translation()
-                .transform_point(&Point3::origin());
-
-            position
-        };
-
-        for edges in graph.edges.iter() {
-            for &Edge { from, to } in edges {
-                let from_position = compute_position(vertex_nodes[from].borrow());
-
-                let to_position = compute_position(vertex_nodes[to].borrow());
-
-                // render an edge.
-                let edge_node = {
-                    let r = EDGE_WIDTH;
-                    let h = nalgebra::distance(&from_position, &to_position);
-
-                    let mut edge_node = window.add_cylinder(r, h);
-
-                    let color = &DEFAULT_EDGE_COLOR;
-                    edge_node.set_color(color[0], color[1], color[2]);
-
-                    let v0 = Vector3::y_axis();
-
-                    // rotation
-                    {
-                        let v1 = to_position - from_position;
-                        let rotation = Rotation3::rotation_between(&v0, &v1).unwrap();
-                        edge_node.append_rotation(&UnitQuaternion::from(rotation));
-                    }
-
-                    // translation
-                    {
-                        let v1: Point3<f32> = (to_position - from_position).into();
-                        edge_node.append_translation(&Translation3::from(from_position));
-                        edge_node.append_translation(&Translation3::from(v1.coords / 2.0));
-                    }
-
-                    edge_node
-                };
-
-                edge_nodes[from].push((to, Rc::new(RefCell::new(edge_node))));
-            }
-        }
-
-        SimulationRenderer {
-            simulation_update_rx: rx,
-            vertex_nodes,
-            edge_nodes,
-            highlighted_edge: None,
-        }
-    }
-}
-
-impl State for SimulationRenderer {
-    fn step(&mut self, _: &mut Window) {
-        if let Ok(update) = self.simulation_update_rx.try_recv() {
-            match update {
-                SimulationUpdate::EdgeSelected(Edge { from, to }) => {
-                    if let Some(edge_node) = self.highlighted_edge.take() {
-                        let color = &DEFAULT_EDGE_COLOR;
-                        edge_node
-                            .borrow_mut()
-                            .set_color(color[0], color[1], color[2]);
-                    }
-
-                    let edge_node = self.edge_nodes[from]
-                        .iter()
-                        .find(|&e| e.0 == to)
-                        .map(|tuple| tuple.1.clone())
-                        .unwrap();
-
-                    let color = &HIGHLIGHT_EDGE_COLOR;
-                    edge_node
-                        .borrow_mut()
-                        .tap_mut(|e| e.set_color(color[0], color[1], color[2]));
-
-                    self.highlighted_edge = Some(edge_node.clone());
-                }
-            }
-        };
-    }
-}
-
 fn main() -> Result<()> {
     color_eyre::install()?;
     env_logger::builder()
@@ -308,12 +75,12 @@ fn main() -> Result<()> {
     let graph = test_graph();
 
     let simulation_thread_handle = std::thread::spawn(move || {
-        let mut simulation = Simulation {
+        let simulation_parameters = SimulationParameters {
             graph,
             simulation_update_sender: tx,
         };
 
-        simulation.start_simulation()
+        SimpleSimulation.start_simulation(simulation_parameters)
     });
 
     let graph = test_graph();
---
+++ src/simulation.rs
@@ -0,0 +1,121 @@
+use std::sync::mpsc::Sender;
+use std::thread::sleep;
+use std::time::Duration;
+
+use color_eyre::Result;
+use log::info;
+use rand::{thread_rng, Rng};
+use tap::Tap;
+
+// ----------------------------------------------------------
+//                    Simulation
+// ----------------------------------------------------------
+
+/// SimulationRenderer should react to these events
+/// and render the simulation accordingly.
+pub(crate) enum SimulationUpdate {
+    HighlightEdge(Edge),
+}
+
+pub(crate) trait Simulation {
+    /// emits SimulationUpdate events.
+    fn start_simulation(self, simulation_parameters: SimulationParameters) -> Result<()>;
+}
+
+pub(crate) struct SimpleSimulation;
+
+pub(crate) struct SimulationParameters {
+    pub(crate) graph: Graph,
+    pub(crate) simulation_update_sender: Sender<SimulationUpdate>,
+}
+
+impl Simulation for SimpleSimulation {
+    fn start_simulation(self, simulation_parameters: SimulationParameters) -> Result<()> {
+        let SimulationParameters {
+            graph,
+            simulation_update_sender,
+        } = simulation_parameters;
+
+        let mut simulation_state = SimulationState {
+            graph,
+            simulation_update_sender,
+        };
+
+        simulation_state.start_simulation()
+    }
+}
+
+struct SimulationState {
+    graph: Graph,
+    simulation_update_sender: Sender<SimulationUpdate>,
+}
+
+impl SimulationState {
+    pub(crate) fn start_simulation(&mut self) -> Result<()> {
+        let mut selected_edge = self.graph.edges[0][0];
+
+        loop {
+            if let Err(_err) = self
+                .simulation_update_sender
+                .send(SimulationUpdate::HighlightEdge(selected_edge))
+            {
+                return Ok(());
+            }
+
+            info!("Highlighted: {:?}", selected_edge);
+            sleep(Duration::from_secs(2));
+
+            let edges_num = self.graph.edges[selected_edge.to].len();
+            let next_edge_idx = thread_rng().gen_range(0..edges_num);
+            let next_edge = self.graph.edges[selected_edge.to][next_edge_idx];
+
+            selected_edge = next_edge;
+        }
+    }
+}
+
+pub(crate) type VertexId = usize;
+
+// might carry some additional data.
+#[derive(Clone, Copy, Debug)]
+pub(crate) struct Edge {
+    pub(crate) from: usize,
+    pub(crate) to: usize,
+}
+
+// might carry some additional data.
+#[derive(Clone, Copy, Debug)]
+#[allow(dead_code)]
+pub(crate) struct Vertex {
+    id: VertexId,
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+pub(crate) struct Graph {
+    pub(crate) vertices: Vec<Vertex>,
+    pub(crate) edges: Vec<Vec<Edge>>,
+}
+
+impl Graph {
+    /// Just a utility, we're probably going to load a graph from a file.
+    /// Might be useful for tests or something.
+    pub(crate) fn construct_graph(
+        n: usize,
+        edges_iter: impl IntoIterator<Item = (usize, usize)>,
+    ) -> Graph {
+        let vertices = (0..n).into_iter().map(|id| Vertex { id }).collect();
+
+        let mut edges = Vec::with_capacity(n).tap_mut(|edges| {
+            for _ in 0..n {
+                edges.push(Vec::new());
+            }
+        });
+
+        for (from, to) in edges_iter.into_iter() {
+            edges[from].push(Edge { from, to });
+        }
+
+        Graph { vertices, edges }
+    }
+}
